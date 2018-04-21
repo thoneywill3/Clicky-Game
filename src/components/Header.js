f21a8e1c37820d199b600f2e7ad56f83e394fbc5
stoversa <stoversa@gmail.com>
2018-04-20 23:35:16 -0400

    Updated components.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => (
   <div className="jumbotron">
-    <h1 className="text-center">Clicky game!</h1>
+    <h1 className="text-center">Click the Fruits!</h1>
     <h3 className="text-center">Click on an image to earn points, but don't click on any more than once!</h3>
   </div>
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,8 @@
-import React, { Component } from "react";
-import Image from "./Image";
-import Img from "./Img.json"
+import React from "react";
 
-class Main extends Component {
-
-  shuffleArray = (array) => {
-
-    let imgArray = Img;
-    for (let i = imgArray.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [imgArray[i], imgArray[j]] = [imgArray[j], imgArray[i]];
-    }
-    return imgArray
-  }
-
-  render() {
-    return (
+const Main = (props) =>
       <div className="container">
-        {this.shuffleArray(Img).map( image => (
-        <Image src={image.url} alt={image.name} key={image.url}/>
-        ))}
+        {props.children}
       </div>
-      )
-    }
-  }
 
 export default Main;
