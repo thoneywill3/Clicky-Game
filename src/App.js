697abbe664a28df3dd61c1a89241178e586038d7
stoversa <stoversa@gmail.com>
2018-04-20 23:35:37 -0400

    Most of game functionality is here

--- src/App.js
+++ src/App.js
@@ -3,15 +3,60 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import Image from "./components/Image";
+import Img from "./components/Img.json"
 import './App.css';
 
 class App extends Component {
+  state = {
+    picked: [],
+    correct: 0,
+    topscore: 0,
+    message: 'Click an image to begin'
+  };
+
+  shuffleArray = (array) => {
+    let imgArray = Img;
+    for (let i = imgArray.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [imgArray[i], imgArray[j]] = [imgArray[j], imgArray[i]];
+    }
+    return imgArray
+  }
+
+  pickImg = (name) => {
+    console.log("Clicked!!");
+    let picked = this.state.picked;
+    
+    if (picked.indexOf(name) === -1) {
+      this.setState({
+        picked: picked.concat(name),
+        correct: this.state.correct + 1,
+        topscore: this.state.correct + 1 > this.state.topscore ? this.state.correct + 1 : this.state.topscore,
+        message: "Correct: Good choice!" 
+      })
+      this.shuffleArray();
+    }
+
+    else {
+      this.setState({
+        message: "Incorrect: Play again?",
+        correct: 0,
+        picked: []
+      })
+    }
+  }
+
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar correct={this.state.correct} topscore={this.state.topscore} message={this.state.message}/>
         <Header />
-        <Main />
+        <Main>
+          {this.shuffleArray(Img).map(image => (
+            <Image src={image.url} name={image.name} key={image.name} pickImg={this.pickImg}  />
+          ))}
+        </Main>
         <Footer />
       </div>
     );
