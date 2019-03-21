4d5b75911ae507f5bf2df9ceda7fdff0c00b3763
thoneywill3 <thoneywill3@gmail.com>
2019-03-21 15:29:12 -0500

    Another Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => (
   <div className="jumbotron p-3">
-    <h1 className="text-center">Click the Fruits!</h1>
-    <h3 className="text-center">Click on an image to earn points, but don't click on any more than once!</h3>
+    <h1 className="text-center">Click the Star Wars Characters!</h1>
+    <h3 className="text-center">Click on an image below to earn points, but don't click on any more than once to keep playing!</h3>
   </div>
 
 )
