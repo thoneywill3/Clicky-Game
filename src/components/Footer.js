1ccd553c164657087434459da8c3d84b2f0c7802
thoneywill3 <thoneywill3@gmail.com>
2019-03-21 16:08:27 -0500

    Another Commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => (
 
 <footer className="footer bg-info">
     <div className="container">
-      <span className="text-dark">Clicky Game - 100% of your daily vitamin C!
+      <span className="text-dark">Clicky Game - Star Wars
       </span>
   </div>
 </footer>
